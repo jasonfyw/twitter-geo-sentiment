d3e4d9036eade50e538ce7c07d4212660498f39c
Jason Wang <jasonwang0610@gmail.com>
2020-08-18 18:15:12 +0200

    Interactive map todisplay sample date

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Axios from 'axios';
+import ReactTooltip from "react-tooltip";
 
-function App() {
-    return (
-        <div className="App">
-         
-        </div>
-    );
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Map from './components/Map';
+
+class App extends Component {
+    state = {
+        sentimentByState: null,
+        tooltipContent: ''
+    }
+
+    setTooltipContent = (content) => {
+        this.setState({ tooltipContent: content })
+        console.log(content)
+    }
+
+    componentDidMount() {
+        Axios.get('http://localhost:3000/analyse_sample_tweets')
+            .then(res => this.setState({ sentimentByState: res.data }));
+    }
+
+
+    render() {
+        // const renderedComponent = (this.state.sentimentByState.length > 0) ? <Map sentimentByState={this.state.sentimentByState} /> : <div /> 
+        // console.log((this.state.sentimentByState.length > 0) ? 'results' : 'no results')
+
+        const response = this.state.sentimentByState;
+
+        if (response === null) { return null }
+
+
+        return (
+            <div className="App">
+                {
+                    !(response === null) && (
+                        <React.Fragment>
+                            <Map sentimentByState={response.results} setTooltipContent={this.setTooltipContent} />
+                            <ReactTooltip>{this.state.tooltipContent}</ReactTooltip>
+                        </React.Fragment>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,128 @@
+import React, { Component, memo } from 'react';
+import { Container } from 'react-bootstrap';
+import { geoCentroid } from "d3-geo";
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Marker,
+    Annotation
+} from "react-simple-maps";
+
+import allStates from "../data/allstates.json";
+
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+
+const offsets = {
+    VT: [50, -8],
+    NH: [34, 2],
+    MA: [30, -1],
+    RI: [28, 2],
+    CT: [35, 10],
+    NJ: [34, 1],
+    DE: [33, 0],
+    MD: [47, 10],
+    DC: [49, 21]
+};
+
+
+
+class Map extends Component {
+
+    state = {
+        sentimentByState: this.props.sentimentByState
+    }
+
+    getSentiment = (id) => {
+        const data = allStates.find(s => s.val === id);
+        if (data) {
+            const fullname = data.fullname;
+
+            const sentimentData = this.state.sentimentByState[fullname];
+            return sentimentData.mean;
+        } else {
+            return null
+        }
+        
+    }
+
+    getStateFullname = (id) => {
+        const data = allStates.find(s => s.val === id);
+        if (data) {
+            const fullname = data.fullname;
+            return fullname
+        } else {
+            return null
+        }
+    }
+
+    getColour = (id) => {
+        const sentiment = this.getSentiment(id)
+
+        if (sentiment === null) {
+            return 'hsl(0, 4%, 72%)'
+        } else {
+            return `hsl(${100 * sentiment}, 75%, 45%)`
+        }
+    }
+
+
+    render() {
+        return (
+            <Container>
+                <ComposableMap data-tip="" projection="geoAlbersUsa">
+                    <Geographies geography={geoUrl}>
+                        {({ geographies }) => (
+                            <>
+                                {geographies.map(geo => (
+                                    <Geography
+                                        key={geo.rsmKey}
+                                        stroke="#FFF"
+                                        geography={geo}
+                                        fill={this.getColour(geo.id)}
+                                        onMouseEnter={() => {
+                                            this.props.setTooltipContent(`${this.getStateFullname(geo.id)} – ${this.getSentiment(geo.id)}`);
+                                        }}
+                                        onMouseLeave={() => {
+                                            this.props.setTooltipContent("");
+                                        }}
+                                    />
+                                ))}
+                                {geographies.map(geo => {
+                                    const centroid = geoCentroid(geo);
+                                    const cur = allStates.find(s => s.val === geo.id);
+                                    return (
+                                        <g key={geo.rsmKey + "-name"}>
+                                            {cur &&
+                                                centroid[0] > -160 &&
+                                                centroid[0] < -67 &&
+                                                (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                                                    <Marker coordinates={centroid}>
+                                                        <text y="2" fontSize={14} textAnchor="middle">
+                                                            {cur.id}
+                                                        </text>
+                                                    </Marker>
+                                                ) : (
+                                                        <Annotation
+                                                            subject={centroid}
+                                                            dx={offsets[cur.id][0]}
+                                                            dy={offsets[cur.id][1]}
+                                                        >
+                                                            <text x={4} fontSize={14} alignmentBaseline="middle">
+                                                                {cur.id}
+                                                            </text>
+                                                        </Annotation>
+                                                    ))}
+                                        </g>
+                                    );
+                                })}
+                            </>
+                        )}
+                    </Geographies>
+                </ComposableMap>
+            </Container>
+        );
+    }
+}
+
+export default memo(Map);
