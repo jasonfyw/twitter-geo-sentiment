b3ce37fbe1dba2f7e51f66257b3ab3e137ae1927
Jason Wang <jasonwang0610@gmail.com>
2020-08-20 23:29:55 +0200

    Removed unused code

--- api/api.py
+++ api/api.py
@@ -2,26 +2,11 @@ from flask import Flask, request
 import numpy as np
 import os
 import pickle
-import twitter
 
 from retrieve_tweets import retrieve_tweets
 from analyse_tweets import analyse_tweets
 
 app = Flask(__name__)
-
-# unpickle pre-trained NB classifier and vectoriser
-clf = pickle.load(open('sentiment_analysis/nb_sentiment_model.p', 'rb'))
-vectoriser = pickle.load(open('sentiment_analysis/vectoriser.p', 'rb'))
-
-# instantiate twitter api object
-# auth using keys from .env file
-api = twitter.Api(
-    consumer_key = os.getenv('TWITTER_CONSUMER_KEY'),
-    consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET'),
-    access_token_key = os.getenv('TWITTER_ACCESS_TOKEN_KEY'),
-    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-)
-
 
 # register blueprint routes
 app.register_blueprint(retrieve_tweets)
