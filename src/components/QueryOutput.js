d2f19011c536142f3bc04a5bcde9cbb918ba34fc
Jason Wang <jasonwang0610@gmail.com>
2020-09-07 00:13:58 +0200

    Removed query fetch

--- src/components/QueryOutput.js
+++ src/components/QueryOutput.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
 import ReactTooltip from "react-tooltip";
-import { Spinner } from 'react-bootstrap';
 
 import Map from './Map';
 
 class QueryOutput extends Component {
     state = {
-        loading: null,
-        data: null,
         tooltipContent: ''
     }
 
@@ -16,45 +12,24 @@ class QueryOutput extends Component {
         this.setState({ tooltipContent: content })
     }
 
-    componentDidMount() {
-        this.setState({ loading: true }, () => {
-            Axios.get('http://localhost:3000/analyse_sample_tweets')
-                .then(res => this.setState({ 
-                    loading: false,
-                    data: res.data 
-                }));
-        })
-    }
-
     render() {
-        const data = this.state.data;
+        const data = this.props.query;
 
         return (
-            <div>
-                {
-                    this.state.loading ? (
-                        <Spinner animation="grow" variant="dark" />
-                    ) : (
-                        !(data === null) && (
-                            <React.Fragment>
-                                <h1>Showing Twitter sentiment by state for '{data.query}'</h1>
-                                <p>Total tweets analysed: {data.total_tweets}</p>
-    
-                                <Map sentimentByState={data.results} setTooltipContent={this.setTooltipContent} />
-    
-                                <p>
-                                    <b>Legend</b> <br />
-                                    <i>1 (green) = positive</i> <br />
-                                    <i>0 (red) = negative</i>
-                                </p>
-    
-                                <ReactTooltip>{this.state.tooltipContent}</ReactTooltip>
-                            </React.Fragment>
-                        )
+            <React.Fragment>
+                <h1>Showing Twitter sentiment by state for '{data.keywords}'</h1>
+                <p>Total tweets analysed: {data.totaltweets}</p>
 
-                    )
-                }
-            </div>
+                <Map sentimentByState={data.sentiment} setTooltipContent={this.setTooltipContent} />
+
+                <p>
+                    <b>Legend</b> <br />
+                    <i>1 (green) = positive</i> <br />
+                    <i>0 (red) = negative</i>
+                </p>
+
+                <ReactTooltip>{this.state.tooltipContent}</ReactTooltip>
+            </React.Fragment>
         );
     }
 }
