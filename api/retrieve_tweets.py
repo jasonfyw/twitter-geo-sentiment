b91a781e3ebba7a564769ef232953087a095cf3a
Jason Wang <jasonwang0610@gmail.com>
2021-02-03 22:18:25 +0100

    Remove unwanted code

--- api/retrieve_tweets.py
+++ api/retrieve_tweets.py
@@ -10,7 +10,6 @@ search_args = load_credentials(
         env_overwrite = False
     )
 
-# states_list = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 states_list = ["California", "Colorado", "Connecticut", "New Jersey", "Delaware", "New York", "Pennsylvania"]
 
 
